61b106c8a6d4e8d3e9fcbe9791579d717eb80ffc
You <dzalolx@gmail.com>
2021-04-24 11:24:41 +0200

    Added cloud kit.

--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -13,11 +13,14 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init() {
-        container = NSPersistentContainer(name: "My_Receipts")
+        container = NSPersistentCloudKitContainer(name: "My_Receipts")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
     }
 }
