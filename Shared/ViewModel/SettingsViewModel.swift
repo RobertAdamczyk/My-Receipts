b9a8043c6b8597da18f7e7257314a77e72ae26a7
You <dzalolx@gmail.com>
2021-05-01 12:00:47 +0200

    Notifications are adding when user change phone.

--- Shared/ViewModel/SettingsViewModel.swift
+++ Shared/ViewModel/SettingsViewModel.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 class SettingsViewModel: ObservableObject {
     @Published var notificationAllowed = false
+    @AppStorage("daysNotification") var daysNotification = 7
     
     func notificationRequest(array: FetchedResults<Receipts>) {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
@@ -28,9 +29,53 @@ class SettingsViewModel: ObservableObject {
     func checkNotifications(array: FetchedResults<Receipts>) {
         UNUserNotificationCenter.current().getPendingNotificationRequests { notifications in
 
-            for notification in notifications {
-                print(notification)
+            if notifications.count == array.count {
+                return
+            }else {
+                UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+                for receipt in array {
+                    self.addNotification(receipt: receipt)
+                }
             }
         }
+    }
+    
+    func addNotification(receipt: Receipts){
+        let content = UNMutableNotificationContent()
+        guard let title = receipt.title else {
+            print("Title error")
+            return
+        }
+        content.title = title
+        content.subtitle = "Za X dni skończy się gwaracja."
+        content.sound = UNNotificationSound.default
+
+        // show this notification five seconds from now
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+        
+        guard let endOfWarranty = receipt.endOfWarranty else {
+            print("EndOfWarranty error")
+            return
+        }
+        let dateNotification = endOfWarranty.addingTimeInterval(TimeInterval(daysNotification * 3600 * -24))
+
+        dateComponents.day = Calendar.current.component(.day, from: dateNotification)
+        dateComponents.month = Calendar.current.component(.month, from: dateNotification)
+        dateComponents.year = Calendar.current.component(.year, from: dateNotification)
+        dateComponents.hour = 22
+        dateComponents.minute = 0
+        // Create the trigger as a repeating event.
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+
+        // choose a random identifier
+        guard let id = receipt.id else {
+            print("ID error")
+            return
+        }
+        let request = UNNotificationRequest(identifier: id.uuidString, content: content, trigger: trigger)
+
+        // add our notification request
+        UNUserNotificationCenter.current().add(request)
     }
 }
