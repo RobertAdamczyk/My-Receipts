f133e5768a3e42f0fb4e22a07f72a82024c3f912
You <dzalolx@gmail.com>
2021-05-04 19:44:51 +0200

    Fixed button in image preview.

--- Shared/View/ImagePreview.swift
+++ Shared/View/ImagePreview.swift
@@ -14,24 +14,29 @@ struct ImagePreview: View {
         ZStack{
             if let image = selectedImage {
                 Color("Layout").opacity( 1 - Double(abs(offset.height) / 700))
-                    .overlay(HStack{
-                        Button(action:{
-                            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-                        }){
-                            Image(systemName: "checkmark")
-                            Text("Save in Photos")
-                        }
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(Color("Blue"))
-                        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-                        .padding(.leading, 10)
-                        .opacity(offset.height == 0 ? 1 : 0)
-                    },alignment: .topLeading)
+                    
                 Image(uiImage: image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .offset(y: offset.height)
                     .scaleEffect(1 - (abs(offset.height) / 1000))
+                Color.clear
+                    .overlay(HStack{
+                        Button(action:{
+                            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                        }){
+                            Text("Save in Photos")
+                        }
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundColor(Color.primary)
+                        .padding(4)
+                        .padding(.horizontal, 3)
+                        .background(RoundedRectangle(cornerRadius: 5)
+                                        .foregroundColor(Color("Light")))
+                        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                        .padding(.leading, 10)
+                        .opacity(offset.height == 0 ? 1 : 0)
+                    },alignment: .topLeading)
             }
         }
         .gesture(
@@ -60,5 +65,6 @@ struct ImagePreview: View {
 struct ImagePreview_Previews: PreviewProvider {
     static var previews: some View {
         ImagePreview(selectedImage: .constant(UIImage(systemName: "person")))
+            .preferredColorScheme(.light)
     }
 }
