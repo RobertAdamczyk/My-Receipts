af05221be094882e1692563394505a91fcec3bb0
You <dzalolx@gmail.com>
2021-05-08 12:40:09 +0200

    Changed view modifier.

--- Shared/ViewModifier/FormFrame.swift
+++ Shared/ViewModifier/FormFrame.swift
@@ -12,14 +12,28 @@ extension View {
         self
             .padding(.horizontal)
             .padding(.vertical, 5)
-            .background(Color("Light"))
-            .overlay(
-                VStack{
-                    Rectangle().frame(height:0.7)
-                    Spacer()
-                    Rectangle().frame(height:0.7)
-                }
-                .foregroundColor(Color("BorderColor"))
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 0.7)
+                    .foregroundColor(Color("BorderColor"))
+            )
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(Color("Light"))
+            )
+            .padding(.horizontal)
+    }
+    
+    var roundedBackgroundWithBorder : some View {
+        self
+            .background(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(lineWidth: 0.7)
+                            .foregroundColor(Color("BorderColor"))
+            )
+            .background(
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(Color("Light"))
             )
     }
 }
