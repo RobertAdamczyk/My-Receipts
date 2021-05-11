519828b8e82a51e5f3ccf49ebaedeb4a1c8fb540
You <dzalolx@gmail.com>
2021-05-11 17:09:25 +0200

    Added categories in receipt list.

--- Shared/View/Receipts/ReceiptsList.swift
+++ Shared/View/Receipts/ReceiptsList.swift
@@ -13,28 +13,45 @@ struct ReceiptsList: View {
     
     var body: some View {
         List{
-            ForEach(coreData.receipts, id: \.self) { receipt in
-                ReceiptRow(receipt: receipt)
-                    .onAppear(){
-                        settingsViewModel.checkNotifications(array: coreData.receipts)
+            if !coreData.categories.isEmpty {
+                Section(header: Text("Categories")){
+                    ForEach(coreData.categories, id: \.self) { categorie in
+                        NavigationLink(destination: Text("123")) {
+                            VStack{
+                                Text(categorie.title ?? "")
+                                    .font(.title3)
+                                    .fontWeight(.semibold)
+                            }
+                            .padding(.vertical, 10)
+                        }
                     }
-            }
-            .onDelete(perform: coreData.removeReceipt)
-            if coreData.receipts.isEmpty {
-                HStack{
-                    Spacer()
-                    Text("You have no receipts.")
-                    Spacer()
-                }
                     
+                }
             }
+            
+            Section(header: Text("Receipts")) {
+                ForEach(coreData.receipts, id: \.self) { receipt in
+                    ReceiptRow(receipt: receipt)
+                        .onAppear(){
+                            settingsViewModel.checkNotifications(array: coreData.receipts)
+                        }
+                }
+                .onDelete(perform: coreData.removeReceipt)
+                if coreData.receipts.isEmpty {
+                    Text("You have no receipts.")
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            
             
            
         }
-        .listStyle(PlainListStyle())
+        .listStyle(GroupedListStyle())
         .navigationTitle("My Receipts")
         .onAppear(){
             settingsViewModel.notificationRequest()
+            coreData.fetchCategories()
             coreData.fetchReceipts()
         }
         .onChange(of: settingsViewModel.notificationAllowed) { value in
