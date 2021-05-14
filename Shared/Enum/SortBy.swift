8464fc773961f369519043057fc216e5a89515c2
You <dzalolx@gmail.com>
2021-05-14 21:31:58 +0200

    Added enum SortBy for sorting receipts.

---
+++ Shared/Enum/SortBy.swift
@@ -0,0 +1,14 @@
+//
+//  SortBy.swift
+//  My Receipts (iOS)
+//
+//  Created by Robert Adamczyk on 14.05.21.
+//
+
+import Foundation
+
+enum SortBy: String {
+    case title = "title"
+    case dateOfPurchase = "dateOfPurchase"
+    case endOfWarranty = "endOfWarranty"
+}
