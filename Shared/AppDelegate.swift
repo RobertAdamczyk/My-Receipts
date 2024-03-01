c108b84b2f8430f7c4ceee688604bc1c04a95240
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-03-01 22:00:24 +0100

    add firebase

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -6,24 +6,13 @@
 //
 
 import UIKit
+import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        #if DEBUG
-        printAppLanguages()
-        #endif
+        FirebaseApp.configure()
         return true
     }
 }
-
-#if DEBUG
-extension AppDelegate {
-
-    private func printAppLanguages() {
-        var availableLanguages = Bundle.main.localizations as [String]
-        print("DEBUG INFO*  AvailableLanguages: \(availableLanguages)")
-    }
-}
-#endif
